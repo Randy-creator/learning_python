87fd05443b3b0a19ae68050f3f7d8532b524ddb7
Randy-creator <hei.randy.2@gmail.com>
2025-04-12 07:40:24 +0300

    Introduction to variables and data types .

---
+++ Basics/2-variables_and_data_types.py
@@ -0,0 +1,6 @@
+name = "Alice"         # String (str)
+age = 25               # Integer (int)
+height = 1.75          # Float (float)
+is_student = True      # Boolean (True or False)
+
+print("I am", name + ",", age, "years old,", height, "meters tall. Student:", is_student)
